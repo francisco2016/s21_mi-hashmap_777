f91c979129cfb6498389e47d246473543afdc76f
francisco2016 <otrayama@gmail.com>
2016-07-20 13:02:13 +0200

    -0735 add mt put() añade un elemento al mapa, o machaca el valor de la clave si ya existía.

--- MiHashMap.java
+++ MiHashMap.java
@@ -17,14 +17,27 @@ public class MiHashMap
     }
 
     /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
+     * Asocia el valor especificado con la clave especificada. Si la clave existía, entonces sobreescribe su valor y
+     * devuelve el valor antiguo. Si no existía devuelve -1.
      */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+    public int put(String clave, int valor){
+        int conSinClave = -1;
+        if(claves.indexOf(clave) == -1){//indexOf() nos devuelve el índice donde se encuentra la clave dada si existe, si nos
+            claves.add(clave);//devuelve -1 quiere decir que la calve no existe y entoces la adjuntamos al mapa con su valor.
+            valores.add(valor);
+        }
+        else{// si la invocación    del mt no es igual a -1 quiere decir que la clave existe, tenemos que devolver su valor y
+            //poner el nuevo valor.
+            int indice = claves.indexOf(clave);//hallo el índice dode está la clave.
+            conSinClave = valores.get(indice);//guardo el valor antiguo de la clave en la VL. para retornarle.
+            valores.set(indice,valor);//set() pone el valor de la clave en el indice señalado.
+        }
+        return conSinClave;
+    } 
+
 }
+
+
+
+
+
