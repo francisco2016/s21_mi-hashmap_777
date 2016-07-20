c67af117a4a5303e5e1476771cd53174db65f9d1
francisco2016 <otrayama@gmail.com>
2016-07-20 12:35:22 +0200

    -0735 creada cl MiHashMap con atributos de tipo ArrayListInt y ArrayListString.

---
+++ MiHashMap.java
@@ -0,0 +1,30 @@
+
+/**
+ * Francisco septiembre.
+ */
+public class MiHashMap
+{
+    private ArrayListInt valores;
+    private ArrayListString claves;
+
+    /**
+     * Constructor for objects of class MiHashMap
+     */
+    public MiHashMap()
+    {
+        valores = new ArrayListInt();
+        claves = new ArrayListString();
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
